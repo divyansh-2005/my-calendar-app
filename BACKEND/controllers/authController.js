f6c710ac749b5f3cde58cda7d2a2cb7953bb94cf
ak3264114 <ak3264114@gmail.com>
2024-10-03 01:52:40 +0530

    added /me route

--- BACKEND/controllers/authController.js
+++ BACKEND/controllers/authController.js
@@ -96,7 +96,22 @@ const loginUser = async (req, res) => {
   }
 };
 
+// authenticated user
+const authenticatedUser = async (req, res) => {
+  try {
+		const userId = req.user.id;
+		const user = await User.findById(userId);
+		if (!user) {
+			return res.status(404).json({message: "User not found" });
+		}
+		return res.status(200).json({ user });
+	} catch (error) {
+		return res.status(500).json({ message: "Internal server error" });
+	}
+};
+
 module.exports = {
   registerUser,
   loginUser,
+  authenticatedUser
 };

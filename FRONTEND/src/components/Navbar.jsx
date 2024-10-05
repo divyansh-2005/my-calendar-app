1d3e50e7c7fd3076eeea0c4224925defcc28aad0
aasiflm10 <aa6125405@gmail.com>
2024-10-05 22:04:24 +0530

    added UserProfile.jsx in frontend issue #21

--- FRONTEND/src/components/Navbar.jsx
+++ FRONTEND/src/components/Navbar.jsx
@@ -6,10 +6,12 @@ const Navbar = () => {
   return (
     <nav className="navbar">
       <div className="navbar-right">
-      <Link to="/" className="navbar-title">CALENDAR APP</Link>
+        <Link to="/" className="navbar-title">CALENDAR APP</Link>
       </div>
       <div className="navbar-left">
-        <button className="profile-button">User Profile</button>
+        <Link to="/profile">
+          <button className="profile-button">User Profile</button>
+        </Link>
       </div>
     </nav>
   );

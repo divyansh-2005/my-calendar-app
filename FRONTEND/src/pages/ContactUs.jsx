59e8b37f943446495ceba056fed8e2ac7446ade3
yeshwanth <yeshwanthds2002@gmail.com>
2024-10-06 23:39:31 +0530

    Few Design Changes for contactus page

--- FRONTEND/src/pages/ContactUs.jsx
+++ FRONTEND/src/pages/ContactUs.jsx
@@ -6,7 +6,6 @@ const ContactUsPage = () => {
         <div>
             <header className="header">
                 <h1>Contact Us</h1>
-             
             </header>
             <main className="contact-content">
                 <section className="contact-section">
@@ -18,20 +17,25 @@ const ContactUsPage = () => {
 
                 <section className="contact-section">
                     <form action="#" method="POST" className="contact-form">
-                        <label htmlFor="name">Your Name</label>
-                        <input type="text" id="name" name="name" required />
-                        
-                        <label htmlFor="email">Your Email</label>
-                        <input type="email" id="email" name="email" required />
-                        
-                        <label htmlFor="message">Your Message</label>
-                        <textarea id="message" name="message" rows="6" required></textarea>
-                        
+                        <div className="form-group">
+                            <label htmlFor="name">Name</label>
+                            <input type="text" id="name" name="name" required />
+                        </div>
+
+                        <div className="form-group">
+                            <label htmlFor="email">Email</label>
+                            <input type="email" id="email" name="email" required />
+                        </div>
+
+                        <div className="form-group">
+                            <label htmlFor="message"> Message</label>
+                            <textarea id="message" name="message" rows="6" required></textarea>
+                        </div>
+
                         <button type="submit" className="submit-button">Send Message</button>
                     </form>
                 </section>
             </main>
-           
         </div>
     );
 };

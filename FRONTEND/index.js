68a3b9875029e26d022291e186f778e19c02da5c
usmaan <2021.mohdusmaan.mogal@ves.ac.in>
2024-10-08 23:54:03 +0530

    Fixed importing issue

---
+++ FRONTEND/index.js
@@ -0,0 +1,11 @@
+import HomePage from "./src/pages/Homepage";
+import Navbar from "./src/components/Navbar";
+import RegisterPage from "./src/pages/RegisterPage";
+import Footer from "./src/components/Footer";
+import UserProfile from "./src/pages/UserProfile";
+import ToDoPage from "./src/pages/ToDoPage";
+import AboutUsPage from "./src/pages/AboutUsPage";
+import ExpensePage from "./src/pages/ExpensePage";
+import ContactUsPage from "./src/pages/ContactUs";
+
+export {HomePage, Navbar , AboutUsPage, ContactUsPage, ToDoPage, ExpensePage, UserProfile,Footer, RegisterPage}

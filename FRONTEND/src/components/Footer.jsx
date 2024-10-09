2efd2745564144357f36d069b1659b5672ec35db
vaibhav kottari <kottarivaibhav@gmail.com>
2024-10-09 19:50:57 +0530

    resolved conflicts

--- FRONTEND/src/components/Footer.jsx
+++ FRONTEND/src/components/Footer.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faFacebook,
@@ -9,30 +10,20 @@ import { Link } from "react-router-dom"; // Import Link
 
 const Footer = () => {
   return (
-    <footer className="w-full grid grid-cols-1 md:items-center lg:grid-cols-3 gap-4 p-4 bg-customGray border-t border-customWhite">
-      <div className="lg:col-span-1 flex flex-col items-center lg:items-start">
-        <p className="text-2xl md:text-3xl lg:text-5xl text-white mb-4 lg:mb-0">LOGO</p>
+    <footer className="grid grid-cols-1 lg:grid-cols-3 gap-4 p-4 bg-customGray border-t border-customWhite ">
+      <div className="lg:col-span-1 flex flex-col items-center lg:items-baseline">
+        <p className="text-5xl">LOGO</p>
         {/* <img src="/path-to-your-logo.png" alt="Logo" className="w-24 h-24 mb-4" /> */}
       </div>
       <div className="lg:col-span-1 flex flex-col items-center">
-        <div className="text-base md:text-justify lg:text-xl text-center text-blue-500 font-bold mb-4">
+        <div className="text-center text-xl text-blue-500 font-bold mb-4">
           &copy; {new Date().getFullYear()} Your Company. All rights reserved.
         </div>
-        <div className="flex flex-col lg:flex-row space-y-2 lg:space-y-0 lg:space-x-2 mb-4">
-          <a
-            href="/about"
-            className="text-sm md:text-base lg:text-lg bg-customGray text-white font-medium transition-colors duration-200 hover:text-blue-500 hover:underline"
         <div className="flex space-x-2 mb-4 ">
           <Link
             to="/AboutUs" 
             className="bg-customGray text-white text-base font-medium transition-colors duration-200 hover:text-blue-500 hover:underline"
           >
-            About Us
-          </a>
-          <span className="text-white hidden lg:inline">|</span>
-          <a
-            href="/contact"
-            className="text-sm md:text-base lg:text-lg bg-customGray text-white font-medium transition-colors duration-200 hover:text-blue-500 hover:underline"
             About Us
           </Link>
           <p>|</p>
@@ -40,12 +31,6 @@ const Footer = () => {
             to="/ContactUs" 
             className="bg-customGray text-white text-base font-medium transition-colors duration-200 hover:text-blue-500 hover:underline"
           >
-            Contact Us
-          </a>
-          <span className="text-white hidden lg:inline">|</span>
-          <a
-            href="/"
-            className="text-sm md:text-base lg:text-lg bg-customGray text-white font-medium transition-colors duration-200 hover:text-blue-500 hover:underline"
             Contact Us
           </Link>
           <p>|</p>
@@ -57,8 +42,8 @@ const Footer = () => {
           </Link>
         </div>
       </div>
-      <div className="lg:col-span-1 flex flex-col items-center lg:items-end">
-        <div className="flex space-x-4 md:space-x-2">
+      <div className="lg:col-span-1 flex flex-col items-end mr-6">
+        <div className="flex space-x-4">
           <a
             href="https://facebook.com"
             target="_blank"

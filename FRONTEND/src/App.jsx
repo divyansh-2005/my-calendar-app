a0a1db1770bccc6dd5a0abac82327b189d8a491f
MANI <chandramanigupta2003@gmail.com>
2024-10-11 01:12:24 +0530

    added dark mode light mode toggle button

--- FRONTEND/src/App.jsx
+++ FRONTEND/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar';
@@ -12,23 +12,38 @@ import { AuthProvider } from './utils/AuthContext';
 import UserProfile from './pages/UserProfile';
 import AboutUsPage from './pages/AboutUsPage';
 import ContactUsPage from './pages/ContactUs';
+import ThemeToggle from './components/themetoggle.jsx';
 
 const App = () => {
   const [selectedDate, setSelectedDate] = useState(new Date());
   const [expenses, setExpenses] = useState([]);
   const [todos, setTodos] = useState([]);
   const [totalExpense, setTotalExpense] = useState(0);
+  const [darkMode, setDarkMode] = useState(false);
 
   const handleDateClick = (date) => {
     setSelectedDate(date);
   };
 
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
+
+  useEffect(() => {
+    if (darkMode) {
+      document.documentElement.classList.add('dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+    }
+  }, [darkMode]);
+
   return (
-  <AuthProvider>  
-    <Router>
-      <div className="App">
-        <Navbar />
-        <main>
+    <AuthProvider>  
+      <Router>
+        <div className={`App ${darkMode ? 'dark' : ''}`}>
+          <Navbar />
+          <ThemeToggle darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
+          <main>
           <Routes>
             <Route
               path="/"
@@ -71,11 +86,11 @@ const App = () => {
             <Route path="/AboutUs" element={<AboutUsPage></AboutUsPage>} /> 
             <Route path="ContactUs" element={<ContactUsPage/>} />
           </Routes>
-        </main>
-        <Footer />
-      </div>
-    </Router>
-  </AuthProvider>
+          </main>
+          <Footer />
+        </div>
+      </Router>
+    </AuthProvider>
   );
 };
 

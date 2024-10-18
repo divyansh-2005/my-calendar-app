e713c4f29b52e4dc01c4ddb5f2260fcb50a3f566
Oindil Golder <oindilgolder303@gmail.com>
2024-10-18 23:33:04 +0530

    fix: login route added in navbar.jsx

--- FRONTEND/src/components/Navbar.jsx
+++ FRONTEND/src/components/Navbar.jsx
@@ -1,26 +1,31 @@
-import React from 'react';
-import '../styles/Navbar.css';
-import { Link } from 'react-router-dom';
+import React from "react";
+import "../styles/Navbar.css";
+import { Link } from "react-router-dom";
 
-
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCalendarAlt, faUserCircle } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCalendarAlt, faUserCircle } from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
   return (
     <nav className="navbar">
-    <div className="navbar-left">
-      <Link to="/" className="navbar-logo">
-        <FontAwesomeIcon icon={faCalendarAlt} size="2x" className="calendar-icon" />
-        <span className="navbar-title">CALENDAR APP</span>
-      </Link>
-    </div>
-    <div className="navbar-right">
-      <button className="profile-button">
-        <FontAwesomeIcon icon={faUserCircle} size="2x" />
-      </button>
-    </div>
-  </nav>
+      <div className="navbar-left">
+        <Link to="/" className="navbar-logo">
+          <FontAwesomeIcon
+            icon={faCalendarAlt}
+            size="2x"
+            className="calendar-icon"
+          />
+          <span className="navbar-title">CALENDAR APP</span>
+        </Link>
+      </div>
+      <div className="navbar-right">
+        <button className="profile-button">
+          <Link to={"/login"}>
+            <FontAwesomeIcon icon={faUserCircle} size="2x" />
+          </Link>
+        </button>
+      </div>
+    </nav>
   );
 };
 
